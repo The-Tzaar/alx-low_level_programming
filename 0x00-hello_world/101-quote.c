554a8c23d8d643fed56fab723930a02592a04ec0
Sandile <situlosandile@gmail.com>
2023-03-19 16:08:10 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char *error_message = "and that piece of art is useful\"
+	char error_message = "\"and that piece of art is useful\"
 		-Dora Korpar, 2015-10-19\n";
 
 	write(STDERR_FILENO, error_message, 59);
