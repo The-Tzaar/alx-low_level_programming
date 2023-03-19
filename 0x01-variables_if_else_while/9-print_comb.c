6a6dca41aad0ce0effaf6899f08ee84b1fdb4d83
Sandile <situlosandile@gmail.com>
2023-03-19 14:58:00 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,46 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints all possible combinations of single-digit numbers separated
+ *        by ", " and in ascending order.
  *
- * Description: Prints all possible combinations of single-digit numbers
- * separated by ", " in ascending order
- *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i, j, k;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
-
-		for (j = i + 1; j < 10; j++)
+		for (j = i; j < 10; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			putchar(',');
-			putchar(' ');
-
-			for (k = j + 1; k < 10; k++)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(k + '0');
-
-				if (i == 7 && j == 8 && k == 9)
-					continue;
-
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(i + 48);
+			putchar(j < 9 ? 44 : 10);
+			putchar(j < 9 ? 32 : 0);
 		}
 	}
-
-	putchar('\n');
 	return (0);
 }
 
