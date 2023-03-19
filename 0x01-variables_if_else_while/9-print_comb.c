459f3ed3bf16691abef61f6cd7c76846aa3c450c
Sandile <situlosandile@gmail.com>
2023-03-19 15:38:35 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,18 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 10; i++) {
-        putchar(i + '0');
-        if (i != 9) {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-    putchar('\n');
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
 
-    return (0);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-
